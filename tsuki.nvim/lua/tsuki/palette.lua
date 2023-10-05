12282112353e4c95a963f8b3eedd53691a18cfba
Winter H. <a2405392127a@gmail.com>
2023-10-05 12:42:07 -0700

    tweaked nvim colors

---
+++ tsuki.nvim/lua/tsuki/palette.lua
@@ -0,0 +1,23 @@
+local colors = {
+  _white = "#ffffff",
+  _l_white = "#ebefff",
+  _d_white = "#c8c8db",
+  _gray_light = "#74778c",
+  _gray = "#606375",
+  _gray_dark = "#535560",
+  _semigray = "#42444b",
+  _lightblack = "#38475e",
+  _semiblack = "#2d384a",
+  _black = "#272d3c",
+  _pink = "#ff6392",
+  _l_pink = "#fd85ac",
+  _yellow = "#ffe45e",
+  _earth = "#e4ae62",
+  _green = "#7fdb9c",
+  _d_turquoise = "#7fdbca",
+  _l_sky = "#7fc8f8",
+  _sky = "#5aa9e6",
+  _purple = "#c792ea",
+}
+
+return colors
